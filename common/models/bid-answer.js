54cc39768627cd2abb191a389f2d3c82ec98e637
Erik Maquera <59391741+Erik-Beckhs@users.noreply.github.com>
2021-12-12 22:22:55 -0400

    dec 12

---
+++ common/models/bid-answer.js
@@ -0,0 +1,12 @@
+'use strict';
+var pubsub = require('../../server/pubsub.js');
+
+module.exports = function(Bidanswer) {
+    Bidanswer.observe('after save', function(ctx, next) {
+        //socket.emit('/Sifeempresa/POST',ctx.instance);
+        pubsub.publish('oferta', ctx.instance);
+          //console.log('> after save triggered:', ctx.Model.modelName, ctx.instance);
+          next();
+      });    
+};
+
